e26666a79cf64f6727fb0cc0b9413e81e176833a
pframpto <42862113+pframpto@users.noreply.github.com>
2019-04-26 17:24:15 +1000

    Add files via upload

---
+++ Convert-EmailAddressToFirstnameLastName.ps1
@@ -0,0 +1,29 @@
+﻿<#
+.SYNOPSIS
+   This script breaks and email address into firstname lastname
+.DESCRIPTION
+   This script breaks and email address into firstname lastname
+
+   If an emailaddress is in the form firstname.lastname@company.com
+   This script will break it up into firstname lastname.
+   The script is intended to illustrate a technique for doing this.
+   It would need to be modified to serve an actual purpose.
+.EXAMPLE
+   Convert-EmailAddressToFirstnameLastName -EmailAddress company.user@company.pri
+
+    The firstname is company
+    The lastname is user
+#>
+Function Convert-EmailAddressToFirstnameLastName {
+    [cmdletbinding()]
+    param(
+        [string]$EmailAddress
+    )
+    $Firstname = $EmailAddress.split(".")[0]
+    $lastnameA = $EmailAddress.split(".")[1]
+    $lastname = $lastnameA.Split("@")[0]
+    Write-Output "The firstname is $Firstname"
+    Write-Output "The lastname is $lastname"
+}
+
+Convert-EmailAddressToFirstnameLastName -EmailAddress company.user@company.pri
---
+++ Set-PDTFullAccessandSendAs.ps1
@@ -0,0 +1,14 @@
+﻿function Set-PDTFullAccessandSendAs {
+    [cmdletbinding()]
+    param(
+        $delegate,
+        $identity
+    )
+    Add-MailboxPermission -Identity $identity -AccessRights "FullAccess" -User $delegate
+    Add-OPMailboxPermission -Identity $identity -AccessRights "FullAccess" -User $delegate
+    Add-OPADPermission -Identity $identiy -User $delegate -AccessRights ExtendedRight -ExtendedRights "send as"
+    Add-RecipientPermission -Identity $identity -AccessRights Sendas -Trustee $delegate -confirm:$false
+
+}
+
+Set-PDTFullAccessandSendAs -identity "ABC@company.com" -delegate "lyn.sim@company.com"
