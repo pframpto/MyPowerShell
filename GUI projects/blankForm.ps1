dde6a954d7eed778bd649b4401183c9e11283293
Paul Frampton <pframpto@hotmail.com>
2021-11-08 06:18:22 +1000

    adding the latest files

---
+++ GUI projects/blankForm.ps1
@@ -0,0 +1,14 @@
+﻿Add-Type -AssemblyName PresentationFramework
+
+[xml]$Form = @"
+    <Window xmlns ="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+    Title="My First Form" Height="480" Width="640">
+    </Window>
+"@
+
+$NR = (New-Object system.xml.xmlnodeReader $Form)
+$win =[Windows.markup.xamlreader]::Load($NR)
+$win.showdialog()
+
+break;
+[System.Windows.MessageBox]::Show("test", "test")
