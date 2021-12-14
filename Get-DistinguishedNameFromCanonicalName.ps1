816dba84718b7ef21cdf5586dd2cb9893e43f8f5
Paul Frampton <pframpto@hotmail.com>
2021-12-14 18:25:23 +1000

    Added Function to convert from cn to dn

---
+++ Get-DistinguishedNameFromCanonicalName.ps1
@@ -0,0 +1,16 @@
+﻿Function Get-DNFromCName {
+    [cmdletbinding()]
+    param(
+        $canonicalName
+    )
+
+    $cns = $canonicalName.Split('/')
+    $countOfbits = $csn.count - 1
+    $lastBit = $cns[$countOfbits]
+
+    (Get-ADUser -Filter {name -eq $lastBit}).distinguishedname
+}
+
+Get-DNFromCName -canonicalName "company.pri/NSW/chris.apperley"
+
+canonical
