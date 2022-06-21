20a44a07d94afc4ffa797248288306a25f9bf114
Paul Frampton <pframpto@hotmail.com>
2022-06-21 12:18:35 +1000

    added an outgridview example to fives

---
+++ Fives/AddingPrintersWOutGridview/Add-PrintersWithOutGridView.ps1
@@ -0,0 +1,8 @@
+﻿$printers = import-csv C:\temp\printers.csv | Out-GridView -Title "Select the printers you want" -PassThru
+
+
+foreach($printer in $printers){
+    $p = $printer.name
+    Write-Verbose "Adding printer $p" -Verbose
+    Add-Printer -ConnectionName $p -WhatIf
+}
