652eb9017daf2227d88647c2d6a2f0231444bfa3
unknown <pframpto@hotmail.com>
2023-05-12 13:24:38 +1000

    added new scripts from mac vm

---
+++ Example_AddingARegistryKey.ps1
@@ -0,0 +1,12 @@
+﻿try{
+    (Get-ItemProperty -Path "HKLM:\SOFTWARE\PaulsUptoStep\" -ErrorAction Stop).step 
+    
+}catch{
+    New-Item -Path "HKLM:\SOFTWARE\" -Name "PaulsUptoStep"
+}
+New-ItemProperty -Path "HKLM:\SOFTWARE\PaulsUptoStep" -Name "Step" -Value "1" -PropertyType DWord -Force
+
+
+break;
+
+Remove-Item -Path "HKLM:\SOFTWARE\PaulsUptoStep"
---
+++ Write-GreenLine.ps1
@@ -0,0 +1,10 @@
+﻿function Write-GreenLine {
+    
+   write-host ('-' * 100) -BackgroundColor Green
+
+
+}
+
+write-greenline 
+"This is some cool text"
+write-greenline
