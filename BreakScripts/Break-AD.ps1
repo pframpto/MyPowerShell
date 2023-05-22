82d880e3bb63b08ce563237c5f4d30facdb7dc79
unknown <administrator@MYCOMPANY.PRI>
2023-05-22 12:53:51 +1000

    finished ad for now

--- BreakScripts/Break-AD.ps1
+++ BreakScripts/Break-AD.ps1
@@ -1,6 +1,7 @@
 ﻿break;
 
-# Create User 
+#region Create User 
+
 $Param = @{
     'Name' = "FirstName.LastName";
     'AccountPassword' = (convertTo-SecureString -AsPlainText "P@ssword" -Force );
@@ -13,14 +14,75 @@ $Param = @{
 }
 New-ADUser @param 
 
-# Create Group
+#endregion
+
+#region Create Group
 
 New-ADGroup -Name 'GroupName' -GroupScope Global -Path "OU=CompanyUsers,DC=mycompany,DC=pri" 
+#endregion
 
-
-# Add user to Group
+#region Add user to Group
 
 Add-ADGroupMember -Identity GroupName -Members "FirstName.LastName"
 
+#endregion
 
-# Create OU
+#region Create OU
+
+New-ADOrganizationalUnit -Name "NewOU" -Path "DC=MyCompany,DC=pri"
+
+#endregion
+
+#region Create Site
+
+New-ADReplicationSite -Description "NSW site sydney office" -Name "NSW"
+
+#endregion
+
+#region Create Subnet
+
+
+New-ADReplicationSubnet -Name "10.10.2.0/24" -Site QLD -Location "Brisbane,QLD" -Description "The Brisbane Office"
+
+#endregion
+
+#region Create GPO
+
+New-GPO -Name NSWUsers -Comment "GPO for NSW Users"
+
+#endregion
+
+#region Link GPO
+
+
+new-GPLink -name NSWUsers -Target "OU=NSW,DC=company,DC=pri" -LinkEnabled Yes
+
+
+#endregion
+
+#region Command names for the GUI tools
+    dsa.msc #Active Directory Users and Computers
+    servermanager # Server Manager
+    domain.msc # AD domains and trusts
+    dsac.exe #Active Director Administrative Center
+    dssite.msc # Active Directory Sites and Services
+
+#endregion
+
+#region install AD
+
+Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
+$param = @{'CreateDnsDelegation'=$false;
+           'DatabasePath'="C:\Windows\NTDS";
+           'DomainName'='company.pri';
+           'DomainNetbiosName'='company';
+           'forestmode'='win2012r2';
+           'installdns'=$true;
+           'logpath'="C:\Windows\NTDS";
+           'NoRebootOnCompletion'=$true;           
+           'confirm'=$false           
+          }
+
+Install-ADDSForest @param -SafeModeAdministratorPassword (convertTo-SecureString -AsPlainText "P@ssw0rd" -Force )
+
+#endregion
