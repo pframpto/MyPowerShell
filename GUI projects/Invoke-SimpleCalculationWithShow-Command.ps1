dac4ff17992212ccd3c367a113f39ca5d54a80ae
unknown <pframpto@hotmail.com>
2023-05-26 15:09:58 +1000

    Added example of using show-command to make gui

---
+++ GUI projects/Invoke-SimpleCalculationWithShow-Command.ps1
@@ -0,0 +1,22 @@
+﻿function invoke-SimpleCalculator {
+    param(
+        
+        [Parameter(Mandatory)]
+        
+        [float]$FirstNumber,
+        [Parameter(Mandatory)]
+        [float]$SecondNumber,
+        [Parameter(Mandatory)]
+        [ValidateSet('Add','Subtract', 'Divide', 'Multiply')]        
+        $Action
+    )
+    switch($Action){
+        "Add"{$FirstNumber + $SecondNumber}
+        "Subtract"{$FirstNumber - $SecondNumber}
+        "Divide"{$FirstNumber / $SecondNumber}
+        "Multiply"{$FirstNumber * $SecondNumber}
+        Default{"Did you forget something?"}
+    }
+}
+
+Invoke-Expression (Show-Command invoke-SimpleCalculator -PassThru ) -ErrorAction Ignore
