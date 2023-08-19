f5039504ceeb8d235e4da3aceb9c9d8cf259c2f9
Paul Frampton <pframpto@hotmail.com>
2023-08-19 12:55:27 +1000

    added break script on defender

---
+++ BreakScripts/Break-Defender.ps1
@@ -0,0 +1,17 @@
+﻿break;
+#working with defender
+$cimsession1 = "W11OnMac"
+Update-MpSignature 
+Update-MpSignature -CimSession $cimsession1
+
+#to start a scan
+Start-MpScan -ScanType QuickScan
+
+#if there were any threats you can find info on them with
+Get-MpThreat
+
+#query defender settings
+Get-MpPreference
+
+#for other defender settings
+Get-Command -Module ConfigDefender
