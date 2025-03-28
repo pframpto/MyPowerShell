ba7df38eeead10e4ab4e1cd10f3bbcbc471eea2b
unknown <administrator@COMPANY.PRI>
2025-03-28 16:02:28 +1000

    add 2025 with reg example

---
+++ 2025/Get-QueryFromRegistryUsingWMI.ps1
@@ -0,0 +1,9 @@
+﻿$computer = "localhost"
+$HKLM = 2147483650
+$key = "SOFTWARE\Microsoft\Windows NT\CurrentVersion"
+$releaseID = "ReleaseId"
+$productName = "ProductName"
+
+$wmi = [wmiclass]"\\$computer\root\default:stdRegProv"
+($wmi.GetStringValue($HKLM,$key,$releaseID)).sValue
+($wmi.GetStringValue($HKLM,$key,$productName)).sValue
