5e230fac708c421ba903632131db977116e7ceee
Paul Frampton <pframpto@hotmail.com>
2025-06-12 16:29:03 +1000

    updated 2025

---
+++ 2025/Measure-DifferentTypesOfArrays.ps1
@@ -0,0 +1,20 @@
+﻿(Measure-Command {
+    $array = @()
+    1..10000 | % {$array += $_}
+}).TotalMilliseconds
+
+
+(Measure-Command {
+    $arrayList = New-Object System.Collections.ArrayList
+    1..10000 | % {$arrayList.Add($_)}
+}).TotalMilliseconds
+
+(Measure-Command {
+    $list = New-Object System.Collections.Generic.List[int]
+    1..10000 | % { $list.Add($_)}
+}).TotalMilliseconds
+
+(Measure-Command {
+    $list = New-Object System.Collections.Generic.List[object]
+    1..10000 | % { $list.Add($_)}
+}).TotalMilliseconds
