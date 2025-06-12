f9043761f74e2e011700a43261665e124dde2de3
Paul Frampton <pframpto@hotmail.com>
2025-06-12 15:22:35 +1000

    added random countries

---
+++ 2025/Get-CitiesFromRandomCountry.ps1
@@ -0,0 +1,3 @@
+﻿$Countries = Invoke-RestMethod -Uri 'https://countriesnow.space/api/v0.1/countries' -Method Get
+$country = $Countries.data.country | Get-Random
+(Invoke-RestMethod -Uri 'https://countriesnow.space/api/v0.1/countries/cities' -Body (@{ country = $country } |ConvertTo-Json -Compress) -Method 'post' -ContentType 'application/json').data
