7cc28d55e9c1a9d4a3d1efe9af27e440a7649bce
pframpto <pframpto@hotmail.com>
2026-02-28 14:48:09 +1000

    added DSC break script

---
+++ BreakScripts/Break-DSC.ps1
@@ -0,0 +1,22 @@
+﻿break;
+
+#region Commands
+
+Disable-DscDebug
+Enable-DscDebug
+Get-DscConfiguration
+Get-DscConfigurationStatus
+Get-DscLocalConfigurationManager
+Get-DscResource
+New-DscChecksum
+Remove-DscConfigurationDocument
+Restore-DscConfiguration
+Stop-DscConfiguration
+Invoke-DscResource
+Publish-DscConfiguration
+Set-DscLocalConfigurationManager
+Start-DscConfiguration
+Test-DscConfiguration
+Update-DscConfiguration
+
+#endregion
