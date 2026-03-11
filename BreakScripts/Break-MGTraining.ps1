f992242f2808612dcac180c52f626487a7454454
pframpto <pframpto@hotmail.com>
2026-03-11 13:52:35 +1000

    Started Break-MMTraining notes from microsoft learn training

---
+++ BreakScripts/Break-MGTraining.ps1
@@ -0,0 +1,13 @@
+﻿break;
+
+get-module -Name microsoft.graph -ListAvailable
+
+Update-Module Microsoft.Graph
+
+Find-MgGraphCommand -command Get-MgUser | Select -First 1 -ExpandProperty Permissions
+
+
+Connect-MgGraph -Scopes "User.Read.All","Group.ReadWrite.All"
+
+get-mguser
+
