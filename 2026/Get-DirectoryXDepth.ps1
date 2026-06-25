25bcb02d3e816437599959b726ab541038f4ecc1
unknown <administrator@COMPANY.PRI>
2026-06-25 17:08:59 +1000

    added code to get directories of a certain length using dot net

---
+++ 2026/Get-DirectoryXDepth.ps1
@@ -0,0 +1,9 @@
+﻿$dirs = @('D:\Curtin\DHS\Shared\ABST')
+$depth = 2
+for ($i = 0; $i -lt $depth; $i++) {
+    $dirs = foreach ($dir in $dirs) {
+        [System.IO.Directory]::EnumerateDirectories($dir)
+    }
+}
+
+$dirs
